d2c2864ede491964b621684188d3a75965ba4659
Christian Mahnke <cmahnke@gmail.com>
2022-01-05 14:53:55 +0100

    Add missing import

--- src/main/java/de/digitalcollections/iiif/hymir/frontend/ViewController.java
+++ src/main/java/de/digitalcollections/iiif/hymir/frontend/ViewController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /** Controller for serving different view pages. */
 @Controller
 public class ViewController {
